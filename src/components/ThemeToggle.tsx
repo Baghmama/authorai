fc391a0207ed95ce55f904eab00d3e31ef2e5fdc
Baghmama <thebaghmama@gmail.com>
2025-09-03 00:42:58 +0530

    Create missing ThemeToggle component

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ThemeToggle: React.FC = () => {
+  return (
+    <div className="flex items-center">
+      <span className="text-sm text-gray-600 dark:text-gray-300">Theme</span>
+    </div>
+  );
+};
+
+export default ThemeToggle;
